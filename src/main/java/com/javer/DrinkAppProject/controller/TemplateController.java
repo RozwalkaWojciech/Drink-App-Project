4dd16a09fc52b26d58e35eefbc6c8ec5dd7c19ce
Wojciech <wojciechrozwalka@gmail.com>
2021-01-07 15:52:52 +0100

    start TemplateController, age query

--- src/main/java/com/javer/DrinkAppProject/controller/TemplateController.java
+++ src/main/java/com/javer/DrinkAppProject/controller/TemplateController.java
@@ -2,9 +2,24 @@ package com.javer.DrinkAppProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TemplateController {
+
+    @GetMapping("/")
+    public String start() {
+        return "age_query";
+    }
+
+    @GetMapping("/adult_check")
+    public String adultCheck(@RequestParam("age") String age) {
+        if (age.equals("18+")) {
+            return "GOOD";
+        } else {
+            return "redirect:goodbye";
+        }
+    }
 
     @GetMapping("/goodbye")
     public String goodbye() {
