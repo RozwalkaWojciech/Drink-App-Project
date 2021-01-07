439f36eb697b363f897d5bf0f77083e1373551ce
Wojciech <wojciechrozwalka@gmail.com>
2021-01-07 14:54:43 +0100

    model user

---
+++ src/main/java/com/javer/DrinkAppProject/model/User.java
@@ -0,0 +1,32 @@
+package com.javer.DrinkAppProject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
