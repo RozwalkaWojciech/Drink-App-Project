750e74c7d9da063cd134c09bba5c8a49c59bc1e6
Wojciech <wojciechrozwalka@gmail.com>
2021-01-07 12:10:46 +0100

    init commit

---
+++ src/main/java/com/javer/DrinkAppProject/DrinkAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javer.DrinkAppProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javer/DrinkAppProject/DrinkAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javer.DrinkAppProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
