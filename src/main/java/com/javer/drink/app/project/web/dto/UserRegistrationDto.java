588e3852e3a8e6241e4ddeecdffb8b1e23844423
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2021-01-11 22:41:47 +0100

    user dto web, register field thymeleaf

---
+++ src/main/java/com/javer/drink/app/project/web/dto/UserRegistrationDto.java
@@ -0,0 +1,18 @@
+package com.javer.drink.app.project.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegistrationDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
