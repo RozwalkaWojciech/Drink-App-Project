bd365bfb2753f18c5daccd975bb75a5e40c6a62e
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2021-01-15 22:42:39 +0100

    add props and exception

---
+++ src/main/java/com/javer/drink/app/project/exception/UploadedFileNotFound.java
@@ -0,0 +1,8 @@
+package com.javer.drink.app.project.exception;
+
+public class UploadedFileNotFound extends Exception{
+
+    public UploadedFileNotFound(String message) {
+        super(message);
+    }
+}
