6d546ef0404580ac782f004ce67f8b7ec6b0db7a
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2021-01-17 16:11:04 +0100

    start to add favourite drink

--- src/main/java/com/javer/drink/app/project/web/controller/DrinkController.java
+++ src/main/java/com/javer/drink/app/project/web/controller/DrinkController.java
@@ -1,6 +1,7 @@
 package com.javer.drink.app.project.web.controller;
 
 import com.javer.drink.app.project.service.DrinkService;
+import com.javer.drink.app.project.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +13,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DrinkController {
 
     private final DrinkService drinkService;
+    private final MessageService messageService;
 
     @GetMapping("/drink")
     public String drinkView(@RequestParam(name = "name") String name, Model model) {
         model.addAttribute("drink", drinkService.get(name));
         model.addAttribute("drinks", drinkService.getAllDrinks());
+        return "drink-view";
+    }
+
+    @GetMapping("favourite-drink")
+    public String favouriteDrink(@RequestParam(name = "name") String name, Model model) {
+//        model.addAttribute("message", messageService.get(2L).getInformation());
         return "drink-view";
     }
 }
