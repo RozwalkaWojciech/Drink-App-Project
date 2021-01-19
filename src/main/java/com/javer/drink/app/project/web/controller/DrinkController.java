b9b5fb9a4adfd3e49d2a119a4cdbb03b5baa1baf
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-01-19 13:01:20 +0100

    fix favourite

--- src/main/java/com/javer/drink/app/project/web/controller/DrinkController.java
+++ src/main/java/com/javer/drink/app/project/web/controller/DrinkController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -29,7 +30,7 @@ public class DrinkController {
         return "drink-view";
     }
 
-    @GetMapping("favourite-drink")
+    @PostMapping("favourite-drink")
     public String favouriteDrink(@RequestParam(name = "name") String name) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         userService.manageFavourite(name, authentication.getName());
