e9cc114db184eb013a9d8bc58c3194d3ec1e8e88
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-16 20:30:55 +0100

    add category to drink class

--- src/main/java/com/javer/drink/app/project/file/mapper/DrinkMapper.java
+++ src/main/java/com/javer/drink/app/project/file/mapper/DrinkMapper.java
@@ -1,7 +1,6 @@
 package com.javer.drink.app.project.file.mapper;
 
 import com.javer.drink.app.project.file.parser.DrinkAPI;
-import com.javer.drink.app.project.model.Category;
 import com.javer.drink.app.project.model.Drink;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,7 +11,7 @@ public class DrinkMapper {
 
     private final IngredientMapper ingredientMapper;
 
-    public Drink mapDrink(DrinkAPI drinkAPI, Category category) {
+    public Drink mapDrink(DrinkAPI drinkAPI) {
         return Drink.builder()
                 .id(drinkAPI.getId())
                 .name(drinkAPI.getName())
@@ -22,7 +21,7 @@ public class DrinkMapper {
                 .ingredientList(ingredientMapper.mapIngredients(drinkAPI))
                 .modificationDate(drinkAPI.getModificationDate())
                 .imageUrl(drinkAPI.getImageUrl())
-                .category(category)
+                .category(drinkAPI.getCategory())
                 .isCustom(false)
                 .isApproved(true)
                 .build();
