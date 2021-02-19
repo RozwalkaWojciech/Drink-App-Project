482ee3ed191c4acbc3450b7f3047b153134116b6
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-19 22:58:12 +0100

    cleaning

--- src/main/java/com/javer/drink/app/project/service/IngredientServiceImpl.java
+++ src/main/java/com/javer/drink/app/project/service/IngredientServiceImpl.java
@@ -4,9 +4,7 @@ import com.javer.drink.app.project.model.Ingredient;
 import com.javer.drink.app.project.repository.IngredientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.RequestScope;
 
-import javax.transaction.Transactional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
